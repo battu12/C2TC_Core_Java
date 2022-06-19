a0e5a6581dd45c4dd96d51f706e64f40ced74098
bhara <bhara@bharath-rog>
2022-06-19 20:26:37 +0530

    commit

---
+++ while_loop/src/while_loop/WhileLoop.java
@@ -0,0 +1,17 @@
+package while_loop;
+import java.util.*;
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println(" Enter a numer  ");
+		int n = sc.nextInt();
+		int i=1;
+		while(i<=n) {
+			System.out.println(i);
+			i++;
+		}
+	}
+
+}
