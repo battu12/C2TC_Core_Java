0df4a30fe5aff12ff434e089a740bb6dfeffa2a7
bhara <bhara@bharath-rog>
2022-06-19 20:50:12 +0530

    commit

---
+++ interface_eg/src/interface_eg/Phone.java
@@ -0,0 +1,7 @@
+package interface_eg;
+
+public interface Phone {
+	public void cost();
+	public void color();
+	public void batteryLife();
+}
---
+++ interface_eg/src/interface_eg/TestPgrm.java
@@ -0,0 +1,13 @@
+package interface_eg;
+
+public class TestPgrm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Iphone obj = new Iphone();
+		obj.cost();
+		obj.color();
+		obj.batteryLife();
+	}
+
+}
