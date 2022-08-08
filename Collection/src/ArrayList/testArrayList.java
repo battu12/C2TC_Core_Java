25a561b73ee2336d89807e435d0f464770969a54
bhara <bhara@bharath-rog>
2022-08-08 23:08:12 +0530

    first commit

---
+++ Collection/src/ArrayList/testArrayList.java
@@ -0,0 +1,27 @@
+package ArrayList;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class testArrayList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList list= new ArrayList ();
+		list.add("BMW");
+		list.add("Audi");
+		list.add("Lambogini");
+		list.add("Ferari");
+		list.add("Landrover");
+		
+		Iterator itr = list.iterator();
+		while(itr.hasNext()) {
+		
+			System.out.println(itr.next());
+			
+		}
+		System.out.println(list.get(4));
+		System.out.println(list.set(1, "Bugati"));
+		System.out.println(list);
+	}
+
+}
---
+++ Collection/src/ArrayListDemo/Student.java
@@ -0,0 +1,13 @@
+package ArrayListDemo;
+import java.util.*;
+
+public class Student {
+	 int id;
+	  String name;
+	
+	public Student(int id,String name) {
+		this.id=id;
+		this.name=name;
+		
+	}
+}
