3930d757174b67371802639c759bc7d9fc8def60
bhara <bhara@bharath-rog>
2022-08-18 16:32:30 +0530

    first commit

---
+++ Collection/src/ArrayListDemo/Student1.java
@@ -0,0 +1,12 @@
+package ArrayListDemo;
+import java.util.*;
+
+public class Student1 {
+	 int st_id;
+	 String sname;
+	
+	public Student1(int st_id,String sname) {
+		this.st_id=st_id;
+		this.sname=sname;
+}
+}
